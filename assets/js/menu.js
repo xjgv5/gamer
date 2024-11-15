289ef8f1c9f9ee7b5db7c22b136cda49dc052789
Javier Gomez <javier.gov@outlook.es>
2024-11-14 22:36:17 -0600

    js submenu responsive

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -13,5 +13,31 @@ document.addEventListener("DOMContentLoaded", (event) => {
             mobile__menu.style.display = "none"
             mobile__menu.classList.remove("menu__open")
         }
+
+        const submenu = (boton, submenu, classname) => {
+            boton.addEventListener("click", () => {
+                let drop = document.querySelector("." + classname)
+                if(!drop){
+                    submenu.style.display = "block"
+                    submenu.classList.add(classname)
+                } else{
+                    submenu.style.display = "none"
+                    submenu.classList.remove(classname)
+                }
+                
+            })
+        }
+
+        // Primer submenu
+        let dropdown1_btn = document.querySelector("#icon1")
+        let dropdown1_submenu = document.querySelector("#submenu1")
+        
+        // Segundo submenu
+        let dropdown2_btn = document.querySelector("#icon2")
+        let dropdown2_submenu = document.querySelector("#submenu2")
+
+        // Ejecutar funcionalidad
+        submenu(dropdown1_btn, dropdown1_submenu, "drop1")
+        submenu(dropdown2_btn, dropdown2_submenu, "drop2")
     })
 })
